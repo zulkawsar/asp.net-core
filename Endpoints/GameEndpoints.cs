35d26281b590e9fbcc61b1f4c4550e9dbd4abfae
zulkawsar <zulkawsar@gmail.com>
2024-01-04 18:42:28 +0600

    Dto use

--- Endpoints/GameEndpoints.cs
+++ Endpoints/GameEndpoints.cs
@@ -1,21 +1,42 @@
-﻿using Games.Entities;
+﻿using Games.Dtos;
+using Games.Entities;
 using Games.Repository;
 
 namespace Games.Endpoints
 {
    public static class GameEndpoints
    {
-        public static RouteGroupBuilder MapGameEnpoint ( this IEndpointRouteBuilder route) {
+        public static RouteGroupBuilder MapGameEnpoint(this IEndpointRouteBuilder route) {
 
+            var GetGameEndpointName = "getGame";
 
             var group = route.MapGroup("/games")
                .WithParameterValidation();
 
-            group.MapGet("/", (IGameRepository gameRepository) => gameRepository.GetGames());
+            group.MapGet("/", (IGameRepository gameRepository) =>
+                gameRepository.GetGames().Select(game => game.AsDtos())
 
-            group.MapGet("/{id}", (IGameRepository gameRepository, int id) => gameRepository.show(id));
+            );
 
-            group.MapPost("/", (IGameRepository gameRepository, Game game) => gameRepository.create(game));
+            group.MapGet("/{id}", (IGameRepository gameRepository, int id) =>
+            {
+                Game? game = gameRepository.show(id);
+                return game is not null ? Results.Ok(game.AsDtos()) : Results.NotFound();
+            }).WithName(GetGameEndpointName);
+
+            group.MapPost("/", (IGameRepository gameRepository, CreateGameDtos gameDto) => 
+            {
+                Game game = new()
+                {
+                    Name = gameDto.Name,
+                    Genre = gameDto.Genre,
+                    Price = gameDto.Price,
+                    ReleaseDate = gameDto.ReleaseDate
+
+                };
+                gameRepository.create(game);
+                return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+            });
 
             group.MapPut("/{id}", (IGameRepository gameRepository, int id, Game gameUpdate) => gameRepository.update(id, gameUpdate));
             return group;
--- obj/Debug/net7.0/Games.AssemblyInfo.cs
+++ obj/Debug/net7.0/Games.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Games")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b01ccab7867ce91dd84675bc7f77b9927533252d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f36c8b5e3aaef92184719945d49382b62b375a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Games")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Games")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
